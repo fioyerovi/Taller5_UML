79392c7bd47f3940e186d3727cb00bd9d26470b3
Fiorella Yerovi <fioreyerovi@gmail.com>
2020-11-18 12:12:06 -0500

    Se agregan clases vacías

---
+++ TallerUML/src/talleruml/vehiculos/Furgoneta.java
@@ -0,0 +1,17 @@
+package talleruml.vehiculos;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author fioye
+ */
+public class Furgoneta {
+    
+}
